21ec3c635874fc042b7186f52055daf90f652083
Sayyed Dilshad Ali <sayyeddilshad26@gmail.com>
2023-11-14 14:10:46 +0530

    first commit

---
+++ palindrome.js
@@ -0,0 +1,16 @@
+function checkPalindrome(string) {
+
+    const arrayValues = string.split('');
+    const reverseArrayValues = arrayValues.reverse();
+    const reverseString = reverseArrayValues.join('');
+
+    if(string == reverseString) {
+        console.log('It is a palindrome');
+    }
+    else {
+        console.log('It is not a palindrome');
+    }
+}
+
+const string = prompt('Enter a string:'+'mom');
+checkPalindrome(string);
